9630649389169c15e6256663525758e1cdbe8fa2
joel <joel@popwi.com>
2013-07-28 12:03:47 -0700

    performing fft in gpu and streaming to network successfully

---
+++ inc/net/popnetworkcomplex.hpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+* Copyright 2013 PopWi Technology Group, Inc. (PTG)
+*
+* This file is proprietary and exclusively owned by PTG or its associates.
+* This document is protected by international and domestic patents where
+* applicable. All rights reserved.
+*
+******************************************************************************/
+
+#include <complex>
+#include <boost/asio.hpp>
+
+#include "core/popsink.hpp"
+
+namespace pop
+{
+
+#define NETWORK_PACKET_SIZE 368 // in samples
+#define NETWORK_STREAM_DATA_TYPE std::complex<float>
+#define NETWORK_PACKET_SIZE_BYTES (sizeof(NETWORK_STREAM_DATA_TYPE) * NETWORK_PACKET_SIZE)
+
+	class PopNetworkComplex : public PopSink<NETWORK_STREAM_DATA_TYPE >
+	{
+	public:
+		PopNetworkComplex(int incoming_port = 5004, int outgoing_port = 35005);
+		~PopNetworkComplex();
+		void process(const NETWORK_STREAM_DATA_TYPE* data, std::size_t size);
+
+	private:
+		void handle_receive(const boost::system::error_code& error,
+	      std::size_t /*bytes_transferred*/);
+		void start_receive();
+		void init();
+
+		boost::asio::ip::udp::socket socket_;
+		int incoming_port_;
+		int outgoing_port_;
+
+		boost::asio::ip::udp::endpoint incoming_endpoint_;
+		boost::asio::ip::udp::endpoint outgoing_endpoint_;
+		std::vector<char> recv_buffer_;
+
+		static boost::asio::io_service io_service;
+	};
+} // namespace pop
---
+++ src/net/popnetworkcomplex.cpp
@@ -0,0 +1,80 @@
+/******************************************************************************
+* Copyright 2013 PopWi Technology Group, Inc. (PTG)
+*
+* This file is proprietary and exclusively owned by PTG or its associates.
+* This document is protected by international and domestic patents where
+* applicable. All rights reserved.
+*
+******************************************************************************/
+
+#include <iostream>
+#include <stdexcept>
+#include <complex>
+
+#include <boost/array.hpp>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include "net/popnetworkcomplex.hpp"
+
+using boost::asio::ip::udp;
+using namespace boost::asio;
+using namespace std;
+
+
+namespace pop
+{
+
+	boost::asio::io_service PopNetworkComplex::io_service;
+
+	PopNetworkComplex::PopNetworkComplex(int incoming_port, int outgoing_port)
+	: PopSink<NETWORK_STREAM_DATA_TYPE >("PopNetworkComplex", NETWORK_PACKET_SIZE), socket_(io_service, udp::endpoint(udp::v4(), incoming_port)),
+	incoming_port_(incoming_port), outgoing_port_(outgoing_port)
+	{
+		cout << "initialized UDP socket" << endl;
+
+		/* We set the outgoing address on the first incoming packet
+		   and set the outgoing port here. */
+		outgoing_endpoint_.address(ip::address::from_string("173.167.119.220"));
+		outgoing_endpoint_.port(outgoing_port);
+	}
+
+	PopNetworkComplex::~PopNetworkComplex()
+	{
+	}
+
+	void PopNetworkComplex::init()
+	{
+	}
+
+	void PopNetworkComplex::start_receive()
+	{
+		socket_.async_receive_from(
+			boost::asio::buffer(recv_buffer_), incoming_endpoint_,
+			boost::bind(&PopNetworkComplex::handle_receive, this,
+				boost::asio::placeholders::error,
+				boost::asio::placeholders::bytes_transferred));
+	}
+
+	void PopNetworkComplex::process(const NETWORK_STREAM_DATA_TYPE* data, size_t len)
+	{
+		socket_.send_to(boost::asio::buffer(data, NETWORK_PACKET_SIZE_BYTES),
+			outgoing_endpoint_);
+	}
+
+	void PopNetworkComplex::handle_receive(const boost::system::error_code& error,
+      std::size_t /*bytes_transferred*/)
+	{
+		cout << "received UDP packet" << endl;
+		//complex<float> *processed_data;
+		if (!error || error == boost::asio::error::message_size)
+		{
+
+			// If a callback function exists then process data and send it.
+			//send( processed_data, recv_buffer_.size() / sizeof( complex<float> ));
+
+			// receive next packet
+			start_receive();
+		}
+	}
+} // namespace pop
