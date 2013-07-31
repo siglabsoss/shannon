8724a6783f30749cd2be8ddfcef7490c9ac8e229
alex <alex@popwi.com>
2013-07-31 15:30:27 -0700

    added code to validate the implementation compatibility between complex<float> and cuComplex

---
+++ tools/src/validate_implementation.cpp
@@ -0,0 +1,58 @@
+#include <complex>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cufft.h>
+
+#define BLACK "\033[22;30m"
+#define RED "\033[22;31m"
+#define GREEN "\033[22;32m"
+#define YELLOW "\033[22;33m"
+#define BLUE "\033[22;34m"
+#define MAGENTA "\033[22;35m"
+#define CYAN "\033[22;36m"
+#define WHITE "\033[22;37m"
+#define RESETCOLOR "\033[0m"
+
+int check_type_compatibility(void)
+{
+	int r = 0;
+
+	float test[2] = {2.0, 3.0};
+	std::complex<float>* s = (std::complex<float>*)&test;
+	cuComplex* c = (cuComplex*)&test;
+
+	printf("Checking complex data type implementation compatibility ------------------------\r\n");
+
+	printf("test[0]=%f, test[1]=%f\r\n", test[0], test[1]);
+
+	printf("complex<float>->real()=%f, complex<float>->imag()=%f\r\n", s->real(), s->imag());
+
+	printf("cuComplex->x=%f, cuComplex->y=%f\r\n", c->x, c->y);
+
+	if( (s->real() != test[0]) || (s->imag() != test[1]) ||
+	    (c->x != test[0]) || (c->y != test[1]) )
+	{
+		printf(RED "cuComplex and std::complex<float> data types are not equivalent!\r\n\r\n" RESETCOLOR);
+		r = -1;
+	}
+	else
+	{
+		printf(GREEN "pass\r\n\r\n" RESETCOLOR);
+	}
+
+
+	return r;
+}
+
+int main(int argc, char *argv[])
+{
+	int r = 0;
+	printf("Validating implementation ------------------------------------------------------\r\n\r\n");
+
+	r &= check_type_compatibility();
+
+	return r;
+}
