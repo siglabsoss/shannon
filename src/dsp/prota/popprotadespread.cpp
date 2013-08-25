c39928827a4ca9d0d84ebfd09c350e7a222d9a25
joel <joel@popwi.com>
2013-08-25 05:43:11 -0700

    added convolution filter coefficient generator as close as possible to matlab script, shannon_calculate_cfc().

--- src/dsp/prota/popprotadespread.cpp
+++ src/dsp/prota/popprotadespread.cpp
@@ -26,10 +26,7 @@
 using namespace std;
 using namespace boost::posix_time;
 
-#define OVERSAMPLE_FACTOR 2
-
-#define PN_NUM_SYMBOLS 400
-#define PN_SIZE (PN_NUM_SYMBOLS * OVERSAMPLE_FACTOR) // in samples
+#define PN_SIZE 512
 #define FFT_SIZE 65536
 #define CHAN_SIZE 1040
 
@@ -38,7 +35,6 @@ using namespace boost::posix_time;
  * CUDA Function Prototypes
  *************************************************************************/
 extern "C" size_t gpu_channel_split(const complex<float> *h_data, complex<float> *out);
-extern "C" size_t gpu_demod(complex<float> *out);
 extern "C" void init_deconvolve(complex<float> *pn, size_t len_pn, size_t len_fft, size_t len_chan);
 extern "C" void cleanup();
 
@@ -57,7 +53,7 @@ namespace pop
 
 	    // Select code here.. 
     	//popGenGMSK(__code_m512_zeros, mp_demod_func, PN_NUM_SYMBOLS, OVERSAMPLE_FACTOR);
-    	popGenGMSK(__code_m512_zeros, mp_demod_func, PN_NUM_SYMBOLS, OVERSAMPLE_FACTOR);
+    	//popGenGMSK(__code_m512_zeros, mp_demod_func, PN_NUM_SYMBOLS, OVERSAMPLE_FACTOR);
 
     	// Init CUDA
 	 	int deviceCount = 0;
@@ -80,146 +76,11 @@ namespace pop
 	        printf("Detected %d CUDA Capable device(s)\n", deviceCount);
 	    }
 
-	    int dev, driverVersion = 0, runtimeVersion = 0;
-
-	    for (dev = 0; dev < deviceCount; ++dev)
-	    {
-	        cudaSetDevice(dev);
-	        cudaDeviceProp deviceProp;
-	        cudaGetDeviceProperties(&deviceProp, dev);
-
-	        printf("\nDevice %d: \"%s\"\n", dev, deviceProp.name);
-
-	        // Console log
-	        cudaDriverGetVersion(&driverVersion);
-	        cudaRuntimeGetVersion(&runtimeVersion);
-	        printf("  CUDA Driver Version / Runtime Version          %d.%d / %d.%d\n", driverVersion/1000, (driverVersion%100)/10, runtimeVersion/1000, (runtimeVersion%100)/10);
-	        printf("  CUDA Capability Major/Minor version number:    %d.%d\n", deviceProp.major, deviceProp.minor);
-
-	        char msg[256];
-	        sprintf(msg, "  Total amount of global memory:                 %.0f MBytes (%llu bytes)\n",
-	                (float)deviceProp.totalGlobalMem/1048576.0f, (unsigned long long) deviceProp.totalGlobalMem);
-	        printf("%s", msg);
-
-	        printf("  (%2d) Multiprocessors x (%3d) CUDA Cores/MP:    %d CUDA Cores\n",
-	               deviceProp.multiProcessorCount,
-	               _ConvertSMVer2Cores(deviceProp.major, deviceProp.minor),
-	               _ConvertSMVer2Cores(deviceProp.major, deviceProp.minor) * deviceProp.multiProcessorCount);
-	        printf("  GPU Clock rate:                                %.0f MHz (%0.2f GHz)\n", deviceProp.clockRate * 1e-3f, deviceProp.clockRate * 1e-6f);
-
-
-	        printf("  Memory Clock rate:                             %.0f Mhz\n", deviceProp.memoryClockRate * 1e-3f);
-	        printf("  Memory Bus Width:                              %d-bit\n",   deviceProp.memoryBusWidth);
-
-	        if (deviceProp.l2CacheSize)
-	        {
-	            printf("  L2 Cache Size:                                 %d bytes\n", deviceProp.l2CacheSize);
-	        }
-
-	        printf("  Max Texture Dimension Size (x,y,z)             1D=(%d), 2D=(%d,%d), 3D=(%d,%d,%d)\n",
-	               deviceProp.maxTexture1D   , deviceProp.maxTexture2D[0], deviceProp.maxTexture2D[1],
-	               deviceProp.maxTexture3D[0], deviceProp.maxTexture3D[1], deviceProp.maxTexture3D[2]);
-	        printf("  Max Layered Texture Size (dim) x layers        1D=(%d) x %d, 2D=(%d,%d) x %d\n",
-	               deviceProp.maxTexture1DLayered[0], deviceProp.maxTexture1DLayered[1],
-	               deviceProp.maxTexture2DLayered[0], deviceProp.maxTexture2DLayered[1], deviceProp.maxTexture2DLayered[2]);
-
-	        printf("  Total amount of constant memory:               %lu bytes\n", deviceProp.totalConstMem);
-	        printf("  Total amount of shared memory per block:       %lu bytes\n", deviceProp.sharedMemPerBlock);
-	        printf("  Total number of registers available per block: %d\n", deviceProp.regsPerBlock);
-	        printf("  Warp size:                                     %d\n", deviceProp.warpSize);
-	        printf("  Maximum number of threads per multiprocessor:  %d\n", deviceProp.maxThreadsPerMultiProcessor);
-	        printf("  Maximum number of threads per block:           %d\n", deviceProp.maxThreadsPerBlock);
-	        printf("  Maximum sizes of each dimension of a block:    %d x %d x %d\n",
-	               deviceProp.maxThreadsDim[0],
-	               deviceProp.maxThreadsDim[1],
-	               deviceProp.maxThreadsDim[2]);
-	        printf("  Maximum sizes of each dimension of a grid:     %d x %d x %d\n",
-	               deviceProp.maxGridSize[0],
-	               deviceProp.maxGridSize[1],
-	               deviceProp.maxGridSize[2]);
-	        printf("  Maximum memory pitch:                          %lu bytes\n", deviceProp.memPitch);
-	        printf("  Texture alignment:                             %lu bytes\n", deviceProp.textureAlignment);
-	        printf("  Concurrent copy and kernel execution:          %s with %d copy engine(s)\n", (deviceProp.deviceOverlap ? "Yes" : "No"), deviceProp.asyncEngineCount);
-	        printf("  Run time limit on kernels:                     %s\n", deviceProp.kernelExecTimeoutEnabled ? "Yes" : "No");
-	        printf("  Integrated GPU sharing Host Memory:            %s\n", deviceProp.integrated ? "Yes" : "No");
-	        printf("  Support host page-locked memory mapping:       %s\n", deviceProp.canMapHostMemory ? "Yes" : "No");
-	        printf("  Alignment requirement for Surfaces:            %s\n", deviceProp.surfaceAlignment ? "Yes" : "No");
-	        printf("  Device has ECC support:                        %s\n", deviceProp.ECCEnabled ? "Enabled" : "Disabled");
-	        printf("  Device supports Unified Addressing (UVA):      %s\n", deviceProp.unifiedAddressing ? "Yes" : "No");
-	        printf("  Device PCI Bus ID / PCI location ID:           %d / %d\n", deviceProp.pciBusID, deviceProp.pciDeviceID);
-
-	        const char *sComputeMode[] =
-	        {
-	            "Default (multiple host threads can use ::cudaSetDevice() with device simultaneously)",
-	            "Exclusive (only one host thread in one process is able to use ::cudaSetDevice() with this device)",
-	            "Prohibited (no host thread can use ::cudaSetDevice() with this device)",
-	            "Exclusive Process (many threads in one process is able to use ::cudaSetDevice() with this device)",
-	            "Unknown",
-	            NULL
-	        };
-	        printf("  Compute Mode:\n");
-	        printf("     < %s >\n", sComputeMode[deviceProp.computeMode]);
-	    }
-
 	    // choose which device to use for this thread
 	    cudaSetDevice(0);
 	    
 	    // allocate CUDA memory
 	    init_deconvolve( mp_demod_func, PN_SIZE, FFT_SIZE, CHAN_SIZE );
-	}
-
-	/**
-	 * Generates DFT matrix.
-	 * @param out must be at least bins*bins samples big
-	 */
-	void PopProtADespread::gen_dft(complex<float>* out, size_t bins)
-	{
-		size_t x, y, idx;
-		
-		for( x = 0; x < bins; x++ )
-		{
-			for( y = 0; y < bins; y++ )
-			{
-				idx = x + y * bins;
-				out[idx].real(cos(-2.0 * M_PI * x * y / bins));
-				out[idx].imag(sin(-2.0 * M_PI * x * y / bins));
-			}
-		}
-	}
-
-	/**
-	 * Generates Inverse DFT matrix.
-	 * @param out must be at least bins*bins samples big
-	 */
-	void PopProtADespread::gen_inv_dft(complex<float>* out, size_t bins)
-	{
-		size_t x, y, idx;
-		
-		for( x = 0; x < bins; x++ )
-		{
-			for( y = 0; y < bins; y++ )
-			{
-				idx = x + y * bins;
-				out[idx].real(cos(2.0 * M_PI * x * y / bins));
-				out[idx].imag(sin(2.0 * M_PI * x * y / bins));
-			}
-		}
-	}
-
-	/**
-	 * Generate carrier
-	 * @param bins vector length in number of samples
-	 * @param harmonic sine wave frequency. Must be multiple of sample frequency
-	 */
-	void PopProtADespread::gen_carrier(complex<float>* out, size_t bins, size_t harmonic)
-	{
-		size_t x;
-
-		for( x = 0; x < bins; x++ )
-		{
-			out[x].real(cos(-2.0 * M_PI * x * harmonic / bins));
-			out[x].imag(sin(-2.0 * M_PI * x * harmonic / bins));
-		}
 	}
 
 
@@ -237,7 +98,7 @@ namespace pop
 
 		//cudaProfilerStart();
 		// call the GPU to process work
-		chan_buf_len = gpu_channel_split(in, out);
+		gpu_channel_split(in, out);
 
 		// process data
 		PopSource<complex<float> >::process();
--- src/dsp/prota/popprotadespread.cu
+++ src/dsp/prota/popprotadespread.cu
@@ -12,9 +12,8 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdio>
+#include <cmath>
 #include "dsp/utils.hpp"
-
-#include <boost/math/common_factor.hpp>
 
 #include <cufft.h>
 
@@ -104,48 +103,6 @@ __global__ void deconvolve(cuComplex *pn, cuComplex *old, cuComplex *in, cuCompl
 
 }
 
-/**
- * pn_matched_filter
- * FIR filter for PN deconvolution. Uses maximum CUDA thread size.
- * Can be used as a partial too.
- * @param in double buffered input data (i.e. twice as long)
- * @param coef filter coefficients - time reversed PN spreading sequence
- * @param out output data
- */
-__global__ void 
-	__launch_bounds(PN_MATCHED_FILTER_THREADS_PER_BLOCK)
-	pn_matched_filter(CuComplex *in, float *coef, CuComplex *out)
-{
-	int i = threadIdx.x;
-	int I = blockDim.x;
-	int n;
-	cuComplex s;
-
-	/// shared memory for PN code
-	extern __shared__ float s_coef[];
-
-	/// allocate vector array big enough to hold old and new data
-	extern __shared__ float4 s_in[];
-
-	/// recast into complex array
-	cuComplex sp_in[] = (cuComplex*)s_in;
-
-	// copy global memory to shared
-	sp_in[i]     = in[i];     ///< double buffer
-	sp_in[i + I] = in[i + I]; ///< new data
-	s_coef[i]      = coef[i]; ///< fitler coefficients
-	__syncthreads();
-
-	// initialize accumulator
-	s.x = 0.0;
-	s.y = 0.0;
-
-	for( n = 0; n < I; n++ ) 
-		s += sp_in[i - n] * s_coef[n];
-
-	out[i] = s;
-}
-
 extern "C"
 {	
 	cuComplex* d_dataa;
@@ -153,10 +110,9 @@ extern "C"
 	cuComplex* d_datac;
 	cuComplex* d_datad;
 	cuComplex* d_datad_upper;
-	cuComplex* d_datae;
-	float*     d_coef;
-	float*     h_coef;
+	cuComplex* d_cfc;
 	cufftHandle plan1;
+	cufftHandle plan2;
 	size_t g_len_chan; ///< length of time series in samples
 	size_t g_len_chan_padded; ///< length of interpolated time series
 	size_t g_len_fft; ///< length of fft in samples
@@ -164,38 +120,150 @@ extern "C"
 	size_t g_start_chan = 16059;
 	size_t g_oversample_rate = 50;
 	size_t g_len_pn_oversampled;
+	/// spreading code m4k_001
+	const int8_t pn[] = {-1, 1, 1,-1,-1, 1, 1, 1,-1, 1, 1, 1, 1,-1, 1,-1,
+				       1, 1, 1, 1, 1,-1, 1,-1,-1,-1,-1, 1, 1, 1,-1,-1,
+				      -1, 1,-1, 1,-1,-1, 1,-1,-1,-1,-1,-1,-1, 1, 1, 1,
+				      -1, 1,-1, 1,-1, 1, 1,-1,-1,-1,-1,-1,-1, 1, 1,-1,
+				      -1,-1,-1,-1, 1,-1,-1,-1,-1, 1,-1, 1, 1, 1,-1,-1,
+				       1,-1, 1, 1, 1, 1, 1, 1, 1, 1, 1,-1,-1, 1,-1,-1,
+				       1, 1, 1,-1, 1,-1,-1,-1, 1,-1, 1,-1, 1, 1, 1,-1,
+				       1,-1,-1,-1, 1,-1,-1,-1, 1, 1,-1, 1, 1, 1,-1, 1,
+				       1,-1,-1,-1,-1, 1, 1, 1, 1,-1, 1,-1, 1,-1, 1,-1,
+				       1,-1, 1,-1, 1, 1, 1, 1, 1,-1,-1, 1, 1,-1, 1, 1,
+				      -1,-1,-1,-1,-1, 1,-1,-1, 1, 1,-1,-1, 1, 1, 1, 1,
+				       1,-1,-1, 1, 1,-1, 1,-1, 1,-1, 1,-1,-1, 1, 1, 1,
+				       1, 1, 1,-1,-1,-1,-1, 1, 1,-1,-1, 1,-1, 1,-1,-1,
+				       1,-1,-1,-1, 1, 1,-1,-1,-1,-1, 1,-1,-1, 1,-1, 1,
+				       1, 1,-1,-1,-1,-1,-1,-1,-1,-1, 1,-1, 1, 1, 1, 1,
+				       1,-1, 1, 1, 1,-1,-1,-1, 1,-1, 1,-1, 1,-1,-1,-1,
+				       1, 1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 1, 1, 1,-1,-1,
+				      -1,-1, 1, 1,-1, 1, 1,-1, 1,-1, 1,-1, 1, 1, 1,-1,
+				      -1, 1,-1,-1, 1, 1,-1, 1,-1, 1, 1,-1, 1, 1, 1,-1,
+				       1,-1, 1, 1, 1, 1, 1,-1,-1,-1,-1, 1, 1, 1, 1, 1,
+				       1,-1,-1, 1, 1,-1,-1, 1,-1,-1,-1,-1, 1, 1,-1, 1,
+				      -1, 1,-1,-1, 1, 1, 1, 1, 1,-1,-1,-1,-1, 1, 1,-1,
+				       1,-1,-1, 1, 1,-1, 1,-1,-1, 1, 1,-1,-1, 1,-1, 1,
+				       1, 1,-1,-1, 1, 1,-1, 1, 1, 1, 1,-1, 1,-1, 1,-1,
+				      -1,-1,-1,-1,-1,-1, 1, 1, 1, 1, 1, 1,-1,-1,-1,-1,
+				       1,-1,-1, 1,-1,-1, 1,-1,-1, 1,-1, 1,-1,-1, 1,-1,
+				       1, 1,-1, 1, 1, 1,-1,-1,-1,-1, 1,-1,-1,-1,-1,-1,
+				       1,-1,-1,-1, 1, 1, 1,-1,-1, 1, 1,-1, 1,-1,-1, 1,
+				       1, 1, 1, 1, 1,-1, 1, 1,-1, 1, 1, 1,-1, 1,-1,-1,
+				       1, 1, 1,-1,-1, 1, 1,-1,-1,-1,-1, 1,-1,-1, 1, 1,
+				      -1,-1, 1,-1, 1, 1,-1,-1, 1, 1, 1,-1,-1, 1, 1, 1,
+				      -1, 1, 1, 1, 1, 1, 1,-1,-1,-1, 1,-1,-1, 1,-1, 1};
 
-	const uint8_t pn_code_b[] = {
-       0x67,0x7A,0xFA,0x1C,0x52,0x07,0x56,0x06,0x08,0x5C,0xBF,0xE4,0xE8,0xAE,0x88,0xDD,
-       0x87,0xAA,0xAF,0x9B,0x04,0xCF,0x9A,0xA7,0xE1,0x94,0x8C,0x25,0xC0,0x2F,0xB8,0xA8,
-       0xC0,0x1C,0x36,0xAE,0x4D,0x6E,0xBE,0x1F,0x99,0x0D,0x4F,0x86,0x9A,0x65,0xCD,0xEA,
-       0x03,0xF0,0x92,0x52,0xDC,0x20,0x8E,0x69,0xFB,0x74,0xE6,0x13,0x2C,0xE7,0x7E,0x25,
-       0xB5,0x78,0xFD,0xFE,0x33,0xAC,0x37,0x2E,0x6B,0x83,0xAC,0xB0,0x22,0x00,0x23,0x97,
-       0xA6,0xEC,0x6F,0xB5,0xBF,0xFC,0xFD,0x4D,0xD4,0xCB,0xF5,0xED,0x1F,0x43,0xFE,0x58,
-       0x23,0xEF,0x4E,0x82,0x32,0xD1,0x52,0xAF,0x0E,0x71,0x8C,0x97,0x05,0x9B,0xD9,0x82};
 
     /**
      * gpu_gen_pn_match_filter_coef
-     * Generates the FIR filter coefficients for the PN matched filter.
-     * @param pn_code spreading code
-     * @param coef PN matched filter coefficients (len=size*oversample_factor)
-     * @param size number of codes
-     * @param oversample_factor number of times oversampled
+     * Generates the convolution filter coefficients for a given PN code.
+     * The cfc is padded in the time domain to allow for discrete convolutions.
+     * @param prn spreading code in format [-1, 1, 1,-1, ...]
+     * @param cfc convolution filter coefficients out (must be 2*osl in length)
+     * @param ncs number of chips per symbol
+     * @param osl oversampled symbol length (should be >= prn length)
+     * @param bt Gaussian filter value. -3dB bandwidth symbol time.
      **/
-    void gpu_gen_pn_match_filter_coef(uint8_t *pn_code, ///< in
-    	                              float* coef,      ///< out
-    	                              unsigned size,
-    	                              unsigned oversample_factor)
+    void gpu_gen_pn_match_filter_coef(const int8_t* prn, complex<float>* cfc,
+    	                              size_t  ncs, size_t osl, float bt)
     {
-    	unsigned m, n, b, B;
+    	size_t n, m;
+    	float* p;  ///< phase
+    	float* fp; ///< filtered phase
+    	complex<float>* y;  ///< matched waveform
+    	complex<float>* yp; ///< interpolated matched waveform
+    	complex<float>* yc; ///< interpolated waveform conjugate
+    	float  t[3] = {-1, 0, 1}; ///< gaussian points
+    	float  h[3]; ///< gaussian filter
+    	float  alpha; ///< gaussian alpha
+    	float  a, b, q;
 
-    	for( n = 0; n < size; n++ )
+    	// allocate buffers
+    	p = (float*) malloc( ncs * sizeof(float) );
+    	fp = (float*) malloc( ncs * sizeof(float) );
+    	y = (complex<float>*) malloc( ncs * sizeof(complex<float>) );
+    	yp = (complex<float>*) malloc( osl * sizeof(complex<float>) );
+    	yc = (complex<float>*) malloc( osl * sizeof(complex<float>) );
+
+    	p[0] = 0; ///< starting phase
+
+    	// generate phase map
+    	for( n = 1; n < ncs; n++ )
     	{
-    		B = n / 8;
-    		b = n % 8;
-    		for( m = 0; m < oversample_factor; m++ )
-    			coef[n + m] = ((pn_code[size - B] >> b) & 0x1)?1.0f:-1.0f;
+    		p[n] = p[n-1] + (float)prn[n] * M_PI / 2;
     	}
+
+    	// gaussian filter
+    	alpha = sqrt( log(2.0) / 2.0 ) / (double)bt;
+    	a = 0.0;
+    	for( n = 0; n < 3; n++ )
+    	{
+    		b = t[n] * M_PI / alpha;
+    		h[n] = sqrt(M_PI) / alpha * exp( -(b * b) );
+    		a += h[n];
+    	}
+
+    	// normalize
+    	for( n = 0; n < 3; n++ )
+    	{
+    		h[n] /= a;
+    	}
+
+    	// filter (convolve) phase map
+    	fp[0] = p[0] * h[1] + p[1] * h[2];
+    	for( n = 1; n < ncs - 1; n++ )
+    	{
+    		fp[n] = p[n-1] * h[0] + p[n] * h[1] + p[n+1] * h[2];
+    	}
+    	fp[ncs] = p[ncs-1] * h[0] + p[ncs] * h[1];
+
+    	// generate
+    	for( n = 0; n < ncs; n++ )
+    	{
+    		y[n].real(cos(fp[n]));
+    		y[n].imag(sin(fp[n]));
+    	}
+
+    	// sinc interpolate to sample frequency
+    	for( m = 0; m < osl; m++ )
+    	{
+    		yp[m] = complex<float>(0.0, 0.0);
+    		for( n = 0; n < ncs; n++ )
+    		{
+    			a = M_PI * ( (float)m / (float)osl * (float)ncs - (float)n );
+    			if( 0 == a )
+    				yp[m] += y[n];
+    			else
+    				yp[m] += sin(a) / a * y[n];
+    		}
+    	}
+
+    	// complex conjugate and flip
+    	for( m = 0; m < osl; m++ )
+    	{
+    		yc[m].real(+yp[osl-m].real());
+    		yc[m].imag(-yp[osl-m].imag());
+    	}
+
+    	// pad and discrete fourier transform
+    	for( m = 0; m < 2 * osl; m++ )
+    	{
+    		cfc[m] = complex<float>(0.0, 0.0);
+    		for( n = 0; n < osl; n++ )
+    		{
+    			q = (float)osl / 4.0 + (float)n; ///< padded index
+    			a = -2.0 * M_PI * (float)m * q / (2.0 * (float)osl);
+    			cfc[m] += yc[n] * complex<float>( cos(a), sin(a) );
+    		}
+    	}
+
+    	// free all dynamically allocated memory
+    	free( p );
+    	free( fp );
+    	free( y );
+    	free( yp );
+    	free( yc );
     }
 
 
@@ -226,7 +294,7 @@ extern "C"
 
 		
 		// chop spectrum up into 50 spreading channels low side-band
-		/*cudaMemcpy(d_datac,
+		cudaMemcpy(d_datac,
 			       d_datab + small_bin_start + small_bin_sideband,
 			       small_bin_sideband * sizeof(cuComplex),
 			       cudaMemcpyDeviceToDevice);
@@ -247,64 +315,19 @@ extern "C"
 		// put back into time domain
 		cufftExecC2C(plan2, (cufftComplex*)d_datac, (cufftComplex*)d_datad_upper, CUFFT_INVERSE);
 		cudaThreadSynchronize();
-  		checkCudaErrors(cudaGetLastError());*/
-
-		// chop spectrum up into 50 spreading channels low side-band
-		cudaMemcpy(d_datac,
-			       d_datab + small_bin_start + small_bin_sideband,
-			       small_bin_sideband * sizeof(cuComplex),
-			       cudaMemcpyDeviceToDevice);
-		// chop spectrum up into 50 spreading channels high side-band
-		cudaMemcpy(d_datac + g_len_fft - small_bin_sideband,
-			       d_datab + small_bin_start,
-			       small_bin_sideband * sizeof(cuComplex),
-			       cudaMemcpyDeviceToDevice);
-
-		// swap double buffer
-		cudaMemcpy(d_datad,
-			       d_datad_upper,
-			       g_len_chan * sizeof(cuComplex),
-			       cudaMemcpyDeviceToDevice);
-		cudaThreadSynchronize();
   		checkCudaErrors(cudaGetLastError());
-
-  		// put back into time domain
-		cufftExecC2C(plan1, (cufftComplex*)d_datac, (cufftComplex*)d_datad_upper, CUFFT_INVERSE);
-		cudaThreadSynchronize();
-  		checkCudaErrors(cudaGetLastError());
-
-  		pn_matched_filter<<<1, PN_MATCHED_FILTER_THREADS_PER_BLOCK, PN_MATCHED_FILTER_THREADS_PER_BLOCK>>>(d_datad, d_coef, d_data_e);
-
 
   		// Copy results to host
-		//cudaMemcpy(out, d_datad_upper, g_len_chan * sizeof(cuComplex), cudaMemcpyDeviceToHost);
+		cudaMemcpy(out, d_datad_upper, g_len_chan * sizeof(cuComplex), cudaMemcpyDeviceToHost);
 		
   		return 0;
-	}
-
-	size_t gpu_demod(complex<float> *out)
-	{
-		/*cuComplex* old_data = d_datad + g_len_chan_padded - h_decon_idx;
-		cuComplex* new_data = old_data + g_len_pn;
-
-  		// deconvolve PN codes
-		//deconvolve<<<10, 80>>>(d_prncode, old_data, new_data, d_datae, g_len_pn);
-		cudaThreadSynchronize();
-		
-		// Copy [deconvolved] results to host
-		cudaMemcpy(out, d_datad, g_len_chan * sizeof(cuComplex), cudaMemcpyDeviceToHost);*/
-
-		return 0;
-	}
-
-	void gpu_pn_matched_filter()
-	{
-		pn_matched_filter<<<1, PN_MATCHED_FILTER_THREADS_PER_BLOCK, PN_MATCHED_FILTER_THREADS_PER_BLOCK>>>();
 	}
 
 
 	void init_deconvolve(complex<float> *h_pn, size_t len_pn, size_t len_fft, size_t len_chan)
 	{
+		complex<float>* h_cfc;
+
 		g_len_chan = len_chan;
 		g_len_chan_padded = len_chan * IFFT_PADDING_FACTOR;
 		g_len_fft = len_fft;
@@ -315,27 +338,25 @@ extern "C"
 		checkCudaErrors(cudaMalloc(&d_dataa, g_len_fft * sizeof(cuComplex)));
 		checkCudaErrors(cudaMalloc(&d_datab, g_len_fft * sizeof(cuComplex)));
 		checkCudaErrors(cudaMalloc(&d_datac, g_len_fft * sizeof(cuComplex)));
-		checkCudaErrors(cudaMalloc(&d_datad, g_len_fft * sizeof(cuComplex)) * 2); // double buffered
+		checkCudaErrors(cudaMalloc(&d_datad, g_len_fft * sizeof(cuComplex) * 2)); // double buffered
+		checkCudaErrors(cudaMalloc(&d_cfc,   g_len_pn  * sizeof(cuComplex) * 2)); // padded
 		d_datad_upper = d_datad + g_len_chan;
-		checkCudaErrors(cudaMalloc(&d_datae, g_len_pn_oversampled * sizeof(cuComplex)));
-
-		checkCudaErrors(cudaMalloc(&d_coef,  g_len_pn_oversampled * sizeof(float)));
-		h_coef = (float*)malloc(g_len_pn_oversampled * sizeof(float)));
 
 		// initialize CUDA memory
 		checkCudaErrors(cudaMemset(d_dataa, 0, g_len_fft * sizeof(cuComplex)));
 		checkCudaErrors(cudaMemset(d_datab, 0, g_len_fft * sizeof(cuComplex)));
 		checkCudaErrors(cudaMemset(d_datac, 0, g_len_fft * sizeof(cuComplex)));
-		checkCudaErrors(cudaMemset(d_datad, 0, g_len_fft * sizeof(cuComplex)) * 2); // dobule buffered
-		//checkCudaErrors(cudaMemset(d_datae, 0, g_len_pn_oversampled * sizeof(cuComplex)));
-
-		gpu_gen_pn_match_filter_coef(pn_code_b, h_coef, g_len_pn, g_oversample_rate)
-		checkCudaErrors(cudaMemcpy(d_coef, h_coef, g_len_pn_oversampled * sizeof(float)));
-		free(h_coef);
-		
+		checkCudaErrors(cudaMemset(d_datad, 0, g_len_fft * sizeof(cuComplex) * 2)); // dobule buffered
 
 	    // setup FFT plans
 	    cufftPlan1d(&plan1, g_len_fft, CUFFT_C2C, 1);
+	    cufftPlan1d(&plan2, g_len_chan, CUFFT_C2C, 1);
+
+	    // generate convolution filter coefficients
+	    h_cfc = (complex<float>*)malloc(2 * g_len_pn * sizeof(complex<float>));
+		gpu_gen_pn_match_filter_coef(pn, h_cfc, 512, 512 /*oversampled*/, 0.5);
+		checkCudaErrors(cudaMemcpy(d_cfc, h_cfc, 2 * g_len_pn * sizeof(cuComplex), cudaMemcpyHostToDevice));
+		free(h_cfc);
 
 	    printf("\n[Popwi::popprotadespread]: init deconvolve complete \n");
 	}
@@ -349,8 +370,7 @@ extern "C"
 	  checkCudaErrors(cudaFree(d_datab));
 	  checkCudaErrors(cudaFree(d_datac));
 	  checkCudaErrors(cudaFree(d_datad));
-	  checkCudaErrors(cudaFree(d_datae));
-	  checkCudaErrors(cudaFree(d_coef));
+	  checkCudaErrors(cudaFree(d_cfc));
 	}
 
 }
