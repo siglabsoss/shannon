eb3d287f642332660f55a1a3a1d04ef291775dc2
Ben Morse <ben@popwi.com>
2013-09-10 02:28:41 -0700

    * converted config to json and added simple test

--- inc/core/config.hpp
+++ inc/core/config.hpp
@@ -5,6 +5,7 @@
 #include <map>
 #include <vector>
 #include <boost/variant.hpp>
+#include "json/json.h"
 
 namespace rbx
 {
@@ -17,37 +18,46 @@ namespace rbx
     static void loadFile( std::string const& filename );
 
     /// set a config variable in the given group
-    template<class T>
-    static void set( std::string const& group, std::string const& var, T const& val );
+//    template<class T>
+//    static void set( std::string const& group, std::string const& var, T const& val );
 
     /// get a config variable from the given group
+//    template<class T>
+//    static T get( std::string const& group, std::string const& var );
+
     template<class T>
-    static T get( std::string const& group, std::string const& var );
+    static T get( std::string const& key );
+
+//    template<class T>
+//    static T get( std::string key );
+
+//    template<class T>
+//    T get( const char* key );
 
     /// create an instance of the class tied to a specific group
-    Config( std::string const& group );
+//    Config( std::string const& group );
 
     /// public destructor
     ~Config() {}
 
     /// set a config variable
     /// this should only be used to set single variables and not vectors
-    template<class T>
-    void set( std::string const& var, T const& val );
-
-    /// get a config variable
-    template<class T>
-    T get( std::string const & var ) const;
+//    template<class T>
+//    void set( std::string const& var, T const& val );
+//
+//    /// get a config variable
+//    template<class T>
+//    T get( std::string const & var ) const;
 
   private:
-    Config() {}
+    Config() : mJsonStorage(0) {}
     Config& operator=( Config const& rhs ) const{}
 
     /// create an instance of the class
     static void init()
     {
-      if(!instance) 
-	instance = new Config();
+    	if(!instance)
+    		instance = new Config();
     }
 
     /// a pointer to an instance of this class, for singleton purposes
@@ -60,42 +70,51 @@ namespace rbx
 
     /// the group that this object is tied to
     std::string mGroup;
+
+    /// holds data
+    json::value mJsonStorage;
   };
 
-  template< class T >
-  void Config::set( std::string const& group, std::string const& var, T const& val )
-  {
-    init();
-    //instance->mVariables[group][var].front() = val;
-    instance->mVariables[group][var] = val;
-  }
+
   
-  template<class T>
-  T Config::get( std::string const& group, std::string const& var )
-  {
-    init();
 
-    if( instance->mVariables.count(group) != 1 )
-      std::cout << "Group does not exist: " << group << std::endl;
 
-    if( instance->mVariables[group].count(var) != 1 )
-    	std::cout << "Variable " << var << " does not exist in group " << std::endl;
 
-    //return instance->mVariables[group][var].front();
-    return boost::get<T>(instance->mVariables[group][var]);
-  }
+//
+//  template<class T>
+//  T get( const char* key )
+//  {
+//	  return get<T>(std::string(key));
+//  }
 
-  template< class T >
-  void Config::set( std::string const& var, T const& val )
-  {
-    set(mGroup, var, val);
-  }
 
-  template< class T >
-  T Config::get( std::string const& var ) const
-  {
-    return get<T>( mGroup, var );
-  }
+
+//  template<class T>
+//  T Config::get( std::string const& group, std::string const& var )
+//  {
+//    init();
+//
+//    if( instance->mVariables.count(group) != 1 )
+//      std::cout << "Group does not exist: " << group << std::endl;
+//
+//    if( instance->mVariables[group].count(var) != 1 )
+//    	std::cout << "Variable " << var << " does not exist in group " << std::endl;
+//
+//    //return instance->mVariables[group][var].front();
+//    return boost::get<T>(instance->mVariables[group][var]);
+//  }
+
+//  template< class T >
+//  void Config::set( std::string const& var, T const& val )
+//  {
+//    set(mGroup, var, val);
+//  }
+
+//  template< class T >
+//  T Config::get( std::string const& var ) const
+//  {
+//    return get<T>( mGroup, var );
+//  }
 
 
 }
--- src/core/config.cpp
+++ src/core/config.cpp
@@ -25,28 +25,37 @@ void Config::loadFile( std::string const& filename )
 {
   init();
 
-//  file_iterator<> first( filename.c_str() ), last;
-//
-//  if (!first)
-//    cout << "Unable to open the config file:" << filename;
-//
-//
-//  last = first.make_end();
+  file_iterator<> first( filename.c_str() ), last;
+
+  if (!first)
+    cout << "Unable to open the config file: " << filename << " (or empty) "<< endl;
+
+
+  last = first.make_end();
+
+
+
+  json::value json = json::parse(first, last);
+
 //  ConfigGrammar< file_iterator<>, qi::rule<file_iterator<> > > parser;
 //
 //  ConfigTree config;
-//  bool parseSucceeded = false;
-//  try
-//    {
+  bool parseSucceeded = false;
+  try
+    {
+	  instance->mJsonStorage = json::parse(first, last);
+
+//	  cout << json::to_string(instance->mJsonStorage["key"]) << endl;
+
 //      parseSucceeded = qi::phrase_parse(first, last, parser, parser.getCommentRule(), config);
-//    }
-//  catch (qi::expectation_failure<file_iterator<> > const& x)
-//    {
-//      std::ostringstream error;
-//      error << "expected: " << x.what_;
+    }
+  catch (std::exception const& x)
+    {
+      std::ostringstream error;
+      error << "expected: " << x.what();
 //      error << ", but got: \"" << std::string(x.first, x.last) << '"' << std::endl;
-//      cout << "Parsing " << error;
-//    }
+      cout << "Parsing " << error;
+    }
 //
 //  if( parseSucceeded )
 //    {
@@ -71,21 +80,43 @@ namespace rbx // template specializations need to be in the cpp file but in the 
     return instance->mVariables[group][var];
   }
   */
+//
+//
+//
+//  template<>
+//  void Config::set<int>( std::string const& group, std::string const& var, int const& val )
+//  {
+////    init();
+////    //instance->mVariables[group][var].front() = val;
+////    instance->mVariables[group][var] = val;
+//  }
 
-#ifdef WITH_QT
   template<>
-  QString Config::get<QString>( std::string const& group, std::string const& var )
+  std::string Config::get<std::string>( std::string const& key )
   {
-    init();
-    std::string str = get<std::string>(group, var);
-    return QString(str.c_str());
+	  init();
+	  return json::to_string(instance->mJsonStorage[key]);
   }
 
-#endif
+  template<>
+  double Config::get<double>( std::string const& key )
+  {
+	  init();
+	  return json::to_number(instance->mJsonStorage[key]);
+  }
+
+  template<>
+  bool Config::get<bool>( std::string const& key )
+  {
+	  init();
+	  return json::to_bool(instance->mJsonStorage[key]);
+  }
+
+
 }
 
-Config::Config( std::string const& group )
-{
-  init();
-  mGroup = group;
-}
+//Config::Config( std::string const& group )
+//{
+//  init();
+//  mGroup = group;
+//}
