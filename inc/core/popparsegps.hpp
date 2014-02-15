5dc98930dcee9d0105d0fdc7d826092035a06f01
Ben Morse <ben@popwi.com>
2014-02-14 19:05:56 -0800

    * parsing and validating checksums

--- inc/core/popparsegps.hpp
+++ inc/core/popparsegps.hpp
@@ -105,15 +105,74 @@ public:
     	}
     }
 
+    unsigned parseHex(std::string &in)
+    {
+    	unsigned result;
+    	std::stringstream ss;
+    	ss << std::hex << in;
+    	ss >> result;
+    	return result;
+    }
+
+    unsigned parseInt(char *in)
+    {
+    	unsigned result;
+    	std::stringstream ss;
+		ss << in;
+		ss >> result;
+		return result;
+    }
+
+    bool checksumOk(std::string &str, unsigned len)
+    {
+    	unsigned givenChecksumInt;
+    	unsigned char givenChecksum;
+    	std::string checkStr = str.substr(len - 3, 2);
+
+    	std::stringstream ss;
+    	ss << std::hex << checkStr;
+    	ss >> givenChecksumInt; // the >> operator must be given an int
+    	givenChecksum = givenChecksumInt;
+
+
+    	// this extracts bytes according to http://www.gpsinformation.org/dale/nmea.htm
+    	std::string checkedBytes = str.substr(1, len - 5);
+
+    	unsigned char calculatedChecksum = 0x00;
+
+    	// loop and xor
+    	for(char& c : checkedBytes) {
+    		calculatedChecksum ^= c;
+    	}
+
+//    	cout << "bytes: " << checkedBytes << endl;
+//    	cout << "checksum: " << (int) checksum << endl;
+//    	cout << "checksum bytes: " << checkStr << endl;
+//    	cout << "given: " << (int) givenChecksumInt << " calculated " << (int) calculatedChecksum << endl;
+
+
+    	return (givenChecksum == calculatedChecksum);
+    }
+
     void parse()
     {
+    	std::size_t found;
+    	bool cok = false;
     	unsigned len = command.size();
     	if( len == 0 )
     		return;
 
+    	// str will contain the entire message, with a trailing \r (from \r\n)
     	std::string str(command.begin(),command.end());
 
-    	std::size_t found;
+    	cok = checksumOk(str, len);
+
+    	if( !cok ) {
+    		cout << "bad GPS checksum" << endl;
+    		return;
+    	}
+
+
 
     	cout << "command: " << str << endl;
 
@@ -135,65 +194,6 @@ public:
     		gga(str);
     	}
 
-//    	const char *json = str.c_str();
-//
-//    	struct json_token arr[POP_JSON_RPC_SUPPORTED_TOKENS];
-//    	const struct json_token *tok, *tok2, *tok3;
-//
-//    	// Tokenize json string, fill in tokens array
-//    	int returnValue = parse_json(json, strlen(json), arr, POP_JSON_RPC_SUPPORTED_TOKENS);
-//
-//    	if( returnValue == JSON_STRING_INVALID || returnValue == JSON_STRING_INCOMPLETE )
-//    	{
-//    		cout << "problem with json string" << endl;
-//    		return;
-//    	}
-//
-//    	if( returnValue == JSON_TOKEN_ARRAY_TOO_SMALL )
-//    	{
-//    		cout << "problem with json string (too many things for us to parse)" << endl;
-//    		return;
-//    	}
-//
-//    	std::string method;
-//    	// Search for parameter "bar" and print it's value
-//    	tok = find_json_token(arr, "method");
-//    	if( !(tok && tok->type == JSON_TYPE_STRING) )
-//    	{
-//    		return;
-//    	}
-//    	else
-//    	{
-//    		method = std::string(tok->ptr, tok->len);
-//    	}
-//
-//
-//    	tok2 = find_json_token(arr, "params");
-//    	if( !(tok2 && tok2->type == JSON_TYPE_ARRAY) )
-//		{
-//    		return;
-//		}
-//
-//    	int methodId = -1;
-//    	tok3 = find_json_token(arr, "id");
-//    	if( !(tok3 && tok3->type == JSON_TYPE_NUMBER) )
-//    	{
-//    		return;
-//    	}
-//    	else
-//    	{
-//    		std::string sval = std::string(tok3->ptr, tok3->len);
-//    		methodId = std::stoi(sval);
-//
-//    		if( methodId < 0 )
-//    			return;
-//    	}
-//
-//
-//
-//
-//    	execute(method, arr, methodId);
-
     }
 
     void gga(std::string &str)
@@ -202,11 +202,18 @@ public:
     	char *token;
     	char *state;
     	unsigned index = 0;
+    	unsigned fixStatus;
 
     	token = strtok_r_single( &str[0], seps, &state );
     	while( token != NULL )
     	{
     		cout << index << ": " << token << endl;
+
+    		if( index == 6 )
+    		{
+    			fixStatus = parseInt(token);
+    			cout << "Fix status: " << fixStatus << endl;
+    		}
 
 
     		/* Do your thing */
