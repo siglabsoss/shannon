acc7e52aedfe1cd0b0fff5815987cc4b1fbe3cbb
Derek Snyder <derek.stephen.snyder@gmail.com>
2014-05-27 17:54:18 -0700

    Improve the accuracy of the fractional seconds numbers in the multilateration test.

--- src/core/popmultilateration_test.cpp
+++ src/core/popmultilateration_test.cpp
@@ -20,21 +20,21 @@ int main()
 	sightings[0].lat = 37.506794;
 	sightings[0].lng = -122.204533;
 	sightings[0].full_secs = 1400556041;
-	sightings[0].frac_secs = 0.0000113805708;
+	sightings[0].frac_secs = 0.000011391297242040692;
 
 	sightings[1].hostname = "hostname_b";
 	sightings[1].tracker_id = 13579;
 	sightings[1].lat = 37.471107;
 	sightings[1].lng = -122.235775;
 	sightings[1].full_secs = 1400556041;
-	sightings[1].frac_secs = 0.0000110584791;
+	sightings[1].frac_secs = 0.000011081226066067346;
 
 	sightings[2].hostname = "hostname_c";
 	sightings[2].tracker_id = 13579;
 	sightings[2].lat = 37.440583;
 	sightings[2].lng = -122.142735;
 	sightings[2].full_secs = 1400556041;
-	sightings[2].frac_secs = 0.0000212043653;
+	sightings[2].frac_secs = 0.000021196070249372317;
 
 	double lat = 0.0, lng = 0.0;
 	multilateration.calculate_location(sightings, &lat, &lng);
