1fd0dce507e1929ed7bd01162d90c387f9d75e53
Derek Snyder <derek.stephen.snyder@gmail.com>
2014-05-27 19:07:50 -0700

    Add a second set of data for testing multilateration. The new data set uses larger distances, so errors are more apparent.

--- src/core/popmultilateration_test.cpp
+++ src/core/popmultilateration_test.cpp
@@ -9,7 +9,9 @@ using pop::PopMultilateration;
 using pop::PopSighting;
 using std::vector;
 
-int main()
+namespace {
+
+void Test1()
 {
 	PopMultilateration multilateration;
 
@@ -41,6 +43,49 @@ int main()
 
 	// Should be "lat == 37.476365 , lng == -122.198803".
 	printf("lat == %f , lng == %f\n", lat, lng);
+}
 
+void Test2()
+{
+	PopMultilateration multilateration;
+
+	vector<PopSighting> sightings(3);
+
+	// Los Angeles
+	sightings[0].hostname = "hostname_a";
+	sightings[0].tracker_id = 13579;
+	sightings[0].lat = 34.0204989;
+	sightings[0].lng = -118.4117325;
+	sightings[0].full_secs = 1400556041;
+	sightings[0].frac_secs = 0.006404773758517968;
+
+	// Winnipeg
+	sightings[1].hostname = "hostname_b";
+	sightings[1].tracker_id = 13579;
+	sightings[1].lat = 49.853822;
+	sightings[1].lng = -97.1522251;
+	sightings[1].full_secs = 1400556041;
+	sightings[1].frac_secs = 0.004024396897936639;
+
+	// Miami
+	sightings[2].hostname = "hostname_c";
+	sightings[2].tracker_id = 13579;
+	sightings[2].lat = 25.782324;
+	sightings[2].lng = -80.2310801;
+	sightings[2].full_secs = 1400556041;
+	sightings[2].frac_secs = 0.013372558268293727;
+
+	double lat = 0.0, lng = 0.0;
+	multilateration.calculate_location(sightings, &lat, &lng);
+
+	// Should be "lat == 51.013117 , lng == -114.0741556" (Calgary).
+	printf("lat == %f , lng == %f\n", lat, lng);
+}
+
+}
+
+int main()
+{
+	Test2();
 	return 0;
 }
