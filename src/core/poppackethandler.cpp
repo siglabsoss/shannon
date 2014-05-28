be7c4a5b7350170695c940e3b1e2afb96dd0a1f8
Ben Morse <ben@popwi.com>
2014-05-27 22:52:57 -0700

    * removing bit-sync from cross correlation math

--- src/core/poppackethandler.cpp
+++ src/core/poppackethandler.cpp
@@ -19,16 +19,14 @@ PopPacketHandler::PopPacketHandler(unsigned notused) : PopSink<uint64_t>("PopPac
 
 }
 
+char dataTx[2] = {0x0f, 0x00};
+
+
 void PopPacketHandler::process(const uint64_t* data, size_t size, const PopTimestamp* timestamp_data, size_t timestamp_size)
 {
 	cout << "got " << size << " samples" << endl;
 
 	uint64_t pitLastSampleTime = data[size-1];
-
-	// This is the bit sync pattern for FF,33,55
-	uint32_t bitSync[] = {0, 21120, 26400, 31680, 36960, 42240, 44880, 47520, 50160, 52800, 55440, 58080, 60720, 63360};
-	uint32_t bitSyncDenseLength = bitSync[ARRAY_LEN(bitSync)-1];
-
 
 
 	uint32_t comb[] = {0, 343200, 559680, 601920, 755040, 813120, 929280, 955680, 997920, 1003200, 1029600, 1135200, 1193280, 1240800, 1251360, 1383360, 1404480, 1483680, 1520640, 1647360, 1694880, 1800480, 1879680, 1921920, 1932480, 1958880, 2085600, 2122560, 2164800, 2180640, 2196480, 2244000, 2344320, 2428800, 2434080, 2476320, 2550240, 2872320, 3067680, 3278880, 3410880, 3669600, 3738240, 3806880, 3838560, 3944160, 3986400, 4134240, 4239840, 4297920, 4345440, 4414080, 4419360, 4593600, 4678080, 4736160, 4878720, 4894560, 5116320, 5221920, 5253600, 5290560, 5512320, 5639040, 5834400, 6019200, 6225120, 6383520, 6452160, 6494400, 6600000, 6668640, 6916800, 7138560, 7170240, 7186080, 7223040, 7275840, 7370880, 7571520, 7587360, 7597920, 7751040, 7898880, 7904160, 7930560, 8110080, 8310720, 8469120, 8500800, 8580000, 8748960, 8880960, 8954880, 8986560, 9086880, 9150240, 9176640, 9229440, 9451200, 9572640, 9625440, 9757440, 9884160, 10047840, 10142880, 10243200};
@@ -85,28 +83,17 @@ void PopPacketHandler::process(const uint64_t* data, size_t size, const PopTimes
 
 	if( prnCodeStart != 0 )
 	{
-
-		uint32_t i, start, end;
 		short flag1 = 0, flag2 = 0;
 		for(i = 1; i < size-1; i++)
 		{
 			if( data2[i] > (prnCodeStart+combDenseLength) && !flag1 )
 			{
 				flag1 = 1;
-				start = i-1;
-			}
-
-			if( data2[i] > (prnCodeStart+combDenseLength+bitSyncDenseLength) && !flag2 )
-			{
-				flag2 = 1;
-				end = MIN(i+1, size-1);
 			}
 		}
 
-		if( !flag1 || !flag2 )
+		if( !flag1 )
 		{
-
-
 			printf("\r\n");
 			for(i=0;i<(size-1);i++)
 			{
@@ -115,20 +102,14 @@ void PopPacketHandler::process(const uint64_t* data, size_t size, const PopTimes
 			}
 			printf("\r\n\r\n");
 
-			printf("data was not longer than comb + bit sync code %d %d\r\n", data2[size-1], (prnCodeStart+combDenseLength+bitSyncDenseLength) );
+			printf("data was not longer than comb %d %d\r\n", data2[size-1], (prnCodeStart+combDenseLength) );
 
 			return;
 		}
 
 
 //		printf("start end %d %d\r\n", start, end);
-
-		bitSyncStart = shannon_pop_correlate(data2+start, (end-start), bitSync, ARRAY_LEN(bitSync), &scoreBitSync);
-
-//		printf("score2: %d\r\n", scoreBitSync);
-
-		printf("Bit sync Start: %d  data start: %d\r\n", bitSyncStart, bitSyncStart+bitSyncDenseLength);
-		printf("prn data start: %d\r\n", prnCodeStart+combDenseLength+bitSyncDenseLength);
+		printf("prn data start: %d\r\n", prnCodeStart+combDenseLength);
 
 
 		uint8_t dataRx[8];
@@ -137,7 +118,7 @@ void PopPacketHandler::process(const uint64_t* data, size_t size, const PopTimes
 //		pop_data_demodulate(data2, size-1, bitSyncStart+bitSyncDenseLength, dataRx, 2, (scorePrn<0?1:0));
 
 		printf("PRN sync method:\r\n");
-		shannon_pop_data_demodulate(data2, size-1, prnCodeStart+combDenseLength+bitSyncDenseLength, dataRx, 8, (scorePrn<0?1:0));
+		shannon_pop_data_demodulate(data2, size-1, prnCodeStart+combDenseLength, dataRx, 8, (scorePrn<0?1:0));
 
 		uint8_t data_decode[2];
 
@@ -170,7 +151,7 @@ void PopPacketHandler::process(const uint64_t* data, size_t size, const PopTimes
 
 			double txDelta = 0.75;
 
-			char dataTx[2] = {0x54, 0x42};
+
 
 			// add .75 seconds
 			uint32_t txTime = (prnCodeStart + (uint32_t)(ARTEMIS_CLOCK_SPEED_HZ*txDelta)) % ARTEMIS_CLOCK_SPEED_HZ;
@@ -182,6 +163,8 @@ void PopPacketHandler::process(const uint64_t* data, size_t size, const PopTimes
 			{
 				rpc->packet_tx(dataTx, 2, txTime, pitTxTime);
 			}
+
+			dataTx[1]++;
 
 //			printf("last pit %llu\r\n", pitLastSampleTime);
 //			printf("calc pit %llu\r\n", pitPrnCodeStart);
