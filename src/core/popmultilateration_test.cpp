e446afa69168042ff093317839f6ef7beb12b554
Derek Snyder <derek.stephen.snyder@gmail.com>
2014-05-30 17:10:40 -0700

    Apply the DRY (Don't Repeat Yourself) principle to the test2 function. This makes the code less vulnerable to copy-and-paste errors.

--- src/core/popmultilateration_test.cpp
+++ src/core/popmultilateration_test.cpp
@@ -58,41 +58,40 @@ void test1()
 
 void test2()
 {
+	static const uint64_t TRACKER_ID = 13579;
+	static const time_t FULL_SECS = 1400556041;
+
 	PopMultilateration multilateration;
 
-	vector<PopSighting> sightings(4);
+	vector<PopSighting> sightings;
 
-	// Los Angeles
-	sightings[0].hostname = "hostname_a";
-	sightings[0].tracker_id = 13579;
-	sightings[0].lat = 34.0204989;
-	sightings[0].lng = -118.4117325;
-	sightings[0].full_secs = 1400556041;
-	sightings[0].frac_secs = 0.006404773758517968;
+	PopSighting sight;
+	sight.tracker_id = TRACKER_ID;
+	sight.full_secs = FULL_SECS;
 
-	// Winnipeg
-	sightings[1].hostname = "hostname_b";
-	sightings[1].tracker_id = 13579;
-	sightings[1].lat = 49.853822;
-	sightings[1].lng = -97.1522251;
-	sightings[1].full_secs = 1400556041;
-	sightings[1].frac_secs = 0.004024396897936639;
+	sight.hostname = "Los Angeles";
+	sight.lat = 34.0204989;
+	sight.lng = -118.4117325;
+	sight.frac_secs = 0.008571202011359472;
+	sightings.push_back(sight);
 
-	// Miami
-	sightings[2].hostname = "hostname_c";
-	sightings[2].tracker_id = 13579;
-	sightings[2].lat = 25.782324;
-	sightings[2].lng = -80.2310801;
-	sightings[2].full_secs = 1400556041;
-	sightings[2].frac_secs = 0.013372558268293727;
+	sight.hostname = "Winnipeg";
+	sight.lat = 49.853822;
+	sight.lng = -97.1522251;
+	sight.frac_secs = 0.004535715127963626;
+	sightings.push_back(sight);
 
-	// Denver
-	sightings[3].hostname = "hostname_d";
-	sightings[3].tracker_id = 13579;
-	sightings[3].lat = 39.7643389;
-	sightings[3].lng = -104.8551115;
-	sightings[3].full_secs = 1400556041;
-	sightings[3].frac_secs = 0.0048069689931959534;
+	sight.hostname = "Miami";
+	sight.lat = 25.782324;
+	sight.lng = -80.2310801;
+	sight.frac_secs = 0.005699991829013924;
+	sightings.push_back(sight);
+
+	sight.hostname = "Denver";
+	sight.lat = 39.7643389;
+	sight.lng = -104.8551115;
+	sight.frac_secs = 0.004223709183504543;
+	sightings.push_back(sight);
 
 	double lat = 0.0, lng = 0.0;
 	multilateration.calculate_location(sightings, &lat, &lng);
