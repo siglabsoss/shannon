siglabsoss/shannon

commit 5ad8a0c4b9af44c1016c351ca54ef868f5d28a5a

* gravitino resets device at 1000000 and 115200 baud, and then sets it to basestation mode every time gravitino starts

inc/core/popserial.hpp
@@ -146,7 +146,17 @@ public:
     {
     	char* buf = rx.get_buffer(1);
 
-    	buf[0] = handle.readChar();
+
+    	try {
+    		buf[0] = handle.readChar();
+    	}
+    	catch (const boost::system::system_error& e) {
+    		cout << "Exception in PopSerial: " << e.what() << endl;
+
+    		// return non-zero because something went wrong
+    		return 1;
+    	}
+
 
 //    	if( buf[0] == '\0')
 //    	{

src/app/gravitino.cpp
@@ -77,6 +77,39 @@ int main(int argc, char *argv[])
 		channel_map.request_block(target_slots-owned_slots);
 	}
 
+	// reset device at baud 1000000
+	{
+		PopArtemisRPC rpc(1);
+		PopSerial uart0("/dev/ttyUSB0", 1000000);
+		rpc.tx.connect(uart0);
+		rpc.send_reset();
+	}
+
+	// reset device at baud 115200
+	{
+		int j = 0;
+		PopArtemisRPC rpc(1);
+		PopSerial uart0("/dev/ttyUSB0", 115200);
+		rpc.tx.connect(uart0);
+		uart0.rx.connect(rpc);
+		uart0.rx.start_thread();
+		rpc.send_reset();
+		boost::posix_time::milliseconds one_second(1000);
+		for(j = 0; j < 4; j++)
+		{
+			boost::this_thread::sleep(one_second);
+			rpc.set_role_base_station();
+		}
+
+		boost::posix_time::milliseconds two_second(2000);
+		boost::this_thread::sleep(two_second);
+
+		if(!rpc.received_basestation_boot())
+		{
+			cout << endl << endl << "attached device did not startup in basestation mode!!!" << endl << endl;
+			boost::this_thread::sleep(one_second);
+		}
+	}
 
 
 

inc/core/popartemisrpc.hpp
@@ -24,10 +24,13 @@ public:
 	void execute(const struct json_token *methodTok, const struct json_token *paramsTok, const struct json_token *idTok, struct json_token arr[POP_JSON_RPC_SUPPORTED_TOKENS], std::string str);
 	void packet_rx(std::string b64_serial, uint32_t offset, double clock_correction);
 	void packet_tx(char* data, uint32_t size, uint32_t txTime, uint64_t pitTxTime);
+	void send_reset();
 	void set_role_base_station();
 	void mock(void);
+	int received_basestation_boot();
 
 	PopPacketHandler* handler;
+	int basestation_boot;
 };
 
 }

inc/core/popsource.hpp
@@ -57,6 +57,11 @@ public:
      */
     ~PopSource()
     {
+    	if( mp_thread )
+    	{
+    		delete mp_thread;
+    	}
+
     	m_buf.free_circular_buffer(m_buf.m_bytesAllocated);
     	m_timestamp_buf.free_circular_buffer(m_timestamp_buf.m_bytesAllocated);
     }

src/core/popartemisrpc.cpp
@@ -36,7 +36,7 @@ namespace pop
 
 
 
-PopArtemisRPC::PopArtemisRPC(unsigned notused) : PopJsonRPC(0), handler(0)
+PopArtemisRPC::PopArtemisRPC(unsigned notused) : PopJsonRPC(0), handler(0), basestation_boot(0)
 {
 }
 
@@ -140,9 +140,11 @@ void PopArtemisRPC::execute(const struct json_token *methodTok, const struct jso
 			buf[127] = '\0';
 			send_rpc(buf, strlen(buf));
 		}
+	}
 
-
-
+	if( method.compare("basestation_boot") == 0 )
+	{
+		basestation_boot = 1;
 	}
 }
 
@@ -226,11 +228,25 @@ void PopArtemisRPC::packet_tx(char* data, uint32_t size, uint32_t txTime, uint64
 
 void PopArtemisRPC::set_role_base_station()
 {
-	static const char RPC_STRING[] =
-		"{ \"method\": \"set_role_base_station\", \"params\": [] }";
+	static const char RPC_STRING[] = "{ \"method\": \"set_role_base_station\", \"params\": [] }";
 
 	// Subtract one from the string size to exclude the trailing '\0' character.
 	send_rpc(RPC_STRING, sizeof(RPC_STRING) - 1);
+}
+
+
+void PopArtemisRPC::send_reset()
+{
+	static const char RPC_STRING[] = "{ \"method\": \"cpu_reset\", \"params\": [] }";
+
+	// Subtract one from the string size to exclude the trailing '\0' character.
+	send_rpc(RPC_STRING, sizeof(RPC_STRING) - 1);
+}
+
+// have we received a "basestation_boot" message?
+int PopArtemisRPC::received_basestation_boot()
+{
+	return basestation_boot;
 }
 
 }

commit 10083c108bc5f34fb4a3f92e6eb3379d8441060f

* totally refactored PopSerial's internal workings.  Can now close serial port reliably * this fixes a random crash introduced in 5ad8a0c4b9af44c1016c351ca54ef868f5d28a5a

3rd/boost_serial/AsyncSerial.cpp
@@ -1,0 +1,313 @@
+/*
+ * File:   AsyncSerial.cpp
+ * Author: Terraneo Federico
+ * Distributed under the Boost Software License, Version 1.0.
+ * Created on September 7, 2009, 10:46 AM
+ *
+ * v1.02: Fixed a bug in BufferedAsyncSerial: Using the default constructor
+ * the callback was not set up and reading didn't work.
+ *
+ * v1.01: Fixed a bug that did not allow to reopen a closed serial port.
+ *
+ * v1.00: First release.
+ *
+ * IMPORTANT:
+ * On Mac OS X boost asio's serial ports have bugs, and the usual implementation
+ * of this class does not work. So a workaround class was written temporarily,
+ * until asio (hopefully) will fix Mac compatibility for serial ports.
+ * 
+ * Please note that unlike said in the documentation on OS X until asio will
+ * be fixed serial port *writes* are *not* asynchronous, but at least
+ * asynchronous *read* works.
+ * In addition the serial port open ignores the following options: parity,
+ * character size, flow, stop bits, and defaults to 8N1 format.
+ * I know it is bad but at least it's better than nothing.
+ *
+ */
+
+#include "AsyncSerial.h"
+
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <boost/bind.hpp>
+
+using namespace std;
+using namespace boost;
+
+//
+//Class AsyncSerial
+//
+
+class AsyncSerialImpl: private boost::noncopyable
+{
+public:
+    AsyncSerialImpl(): io(), port(io), backgroundThread(), open(false),
+            error(false) {}
+
+    boost::asio::io_service io; ///< Io service object
+    boost::asio::serial_port port; ///< Serial port object
+    boost::thread backgroundThread; ///< Thread that runs read/write operations
+    bool open; ///< True if port open
+    bool error; ///< Error flag
+    mutable boost::mutex errorMutex; ///< Mutex for access to error
+
+    /// Data are queued here before they go in writeBuffer
+    std::vector<char> writeQueue;
+    boost::shared_array<char> writeBuffer; ///< Data being written
+    size_t writeBufferSize; ///< Size of writeBuffer
+    boost::mutex writeQueueMutex; ///< Mutex for access to writeQueue
+    char readBuffer[AsyncSerial::readBufferSize]; ///< data being read
+
+    /// Read complete callback
+    boost::function<void (const char*, size_t)> callback;
+};
+
+AsyncSerial::AsyncSerial(): pimpl(new AsyncSerialImpl)
+{
+
+}
+
+AsyncSerial::AsyncSerial(const std::string& devname, unsigned int baud_rate,
+        asio::serial_port_base::parity opt_parity,
+        asio::serial_port_base::character_size opt_csize,
+        asio::serial_port_base::flow_control opt_flow,
+        asio::serial_port_base::stop_bits opt_stop)
+        : pimpl(new AsyncSerialImpl)
+{
+    open(devname,baud_rate,opt_parity,opt_csize,opt_flow,opt_stop);
+}
+
+void AsyncSerial::open(const std::string& devname, unsigned int baud_rate,
+        asio::serial_port_base::parity opt_parity,
+        asio::serial_port_base::character_size opt_csize,
+        asio::serial_port_base::flow_control opt_flow,
+        asio::serial_port_base::stop_bits opt_stop)
+{
+    if(isOpen()) close();
+
+    setErrorStatus(true);//If an exception is thrown, error_ remains true
+    pimpl->port.open(devname);
+    pimpl->port.set_option(asio::serial_port_base::baud_rate(baud_rate));
+    pimpl->port.set_option(opt_parity);
+    pimpl->port.set_option(opt_csize);
+    pimpl->port.set_option(opt_flow);
+    pimpl->port.set_option(opt_stop);
+
+    //This gives some work to the io_service before it is started
+    pimpl->io.post(boost::bind(&AsyncSerial::doRead, this));
+
+    thread t(boost::bind(&asio::io_service::run, &pimpl->io));
+    pimpl->backgroundThread.swap(t);
+    setErrorStatus(false);//If we get here, no error
+    pimpl->open=true; //Port is now open
+}
+
+bool AsyncSerial::isOpen() const
+{
+    return pimpl->open;
+}
+
+bool AsyncSerial::errorStatus() const
+{
+    lock_guard<mutex> l(pimpl->errorMutex);
+    return pimpl->error;
+}
+
+void AsyncSerial::close()
+{
+    if(!isOpen()) return;
+
+    pimpl->open=false;
+    pimpl->io.post(boost::bind(&AsyncSerial::doClose, this));
+    pimpl->backgroundThread.join();
+    pimpl->io.reset();
+    if(errorStatus())
+    {
+        throw(boost::system::system_error(boost::system::error_code(),
+                "Error while closing the device"));
+    }
+}
+
+void AsyncSerial::write(const char *data, size_t size)
+{
+    {
+        lock_guard<mutex> l(pimpl->writeQueueMutex);
+        pimpl->writeQueue.insert(pimpl->writeQueue.end(),data,data+size);
+    }
+    pimpl->io.post(boost::bind(&AsyncSerial::doWrite, this));
+}
+
+void AsyncSerial::write(const std::vector<char>& data)
+{
+    {
+        lock_guard<mutex> l(pimpl->writeQueueMutex);
+        pimpl->writeQueue.insert(pimpl->writeQueue.end(),data.begin(),
+                data.end());
+    }
+    pimpl->io.post(boost::bind(&AsyncSerial::doWrite, this));
+}
+
+void AsyncSerial::writeString(const std::string& s)
+{
+    {
+        lock_guard<mutex> l(pimpl->writeQueueMutex);
+        pimpl->writeQueue.insert(pimpl->writeQueue.end(),s.begin(),s.end());
+    }
+    pimpl->io.post(boost::bind(&AsyncSerial::doWrite, this));
+}
+
+AsyncSerial::~AsyncSerial()
+{
+    if(isOpen())
+    {
+        try {
+            close();
+        } catch(...)
+        {
+            //Don't throw from a destructor
+        }
+    }
+}
+
+void AsyncSerial::doRead()
+{
+    pimpl->port.async_read_some(asio::buffer(pimpl->readBuffer,readBufferSize),
+            boost::bind(&AsyncSerial::readEnd,
+            this,
+            asio::placeholders::error,
+            asio::placeholders::bytes_transferred));
+}
+
+void AsyncSerial::readEnd(const boost::system::error_code& error,
+        size_t bytes_transferred)
+{
+    if(error)
+    {
+        #ifdef __APPLE__
+        if(error.value()==45)
+        {
+            //Bug on OS X, it might be necessary to repeat the setup
+            //http://osdir.com/ml/lib.boost.asio.user/2008-08/msg00004.html
+            doRead();
+            return;
+        }
+        #endif //__APPLE__
+        //error can be true even because the serial port was closed.
+        //In this case it is not a real error, so ignore
+        if(isOpen())
+        {
+            doClose();
+            setErrorStatus(true);
+        }
+    } else {
+        if(pimpl->callback) pimpl->callback(pimpl->readBuffer,
+                bytes_transferred);
+        doRead();
+    }
+}
+
+void AsyncSerial::doWrite()
+{
+    //If a write operation is already in progress, do nothing
+    if(pimpl->writeBuffer==0)
+    {
+        lock_guard<mutex> l(pimpl->writeQueueMutex);
+        pimpl->writeBufferSize=pimpl->writeQueue.size();
+        pimpl->writeBuffer.reset(new char[pimpl->writeQueue.size()]);
+        copy(pimpl->writeQueue.begin(),pimpl->writeQueue.end(),
+                pimpl->writeBuffer.get());
+        pimpl->writeQueue.clear();
+        async_write(pimpl->port,asio::buffer(pimpl->writeBuffer.get(),
+                pimpl->writeBufferSize),
+                boost::bind(&AsyncSerial::writeEnd, this, asio::placeholders::error));
+    }
+}
+
+void AsyncSerial::writeEnd(const boost::system::error_code& error)
+{
+    if(!error)
+    {
+        lock_guard<mutex> l(pimpl->writeQueueMutex);
+        if(pimpl->writeQueue.empty())
+        {
+            pimpl->writeBuffer.reset();
+            pimpl->writeBufferSize=0;
+            
+            return;
+        }
+        pimpl->writeBufferSize=pimpl->writeQueue.size();
+        pimpl->writeBuffer.reset(new char[pimpl->writeQueue.size()]);
+        copy(pimpl->writeQueue.begin(),pimpl->writeQueue.end(),
+                pimpl->writeBuffer.get());
+        pimpl->writeQueue.clear();
+        async_write(pimpl->port,asio::buffer(pimpl->writeBuffer.get(),
+                pimpl->writeBufferSize),
+                boost::bind(&AsyncSerial::writeEnd, this, asio::placeholders::error));
+    } else {
+        setErrorStatus(true);
+        doClose();
+    }
+}
+
+void AsyncSerial::doClose()
+{
+    boost::system::error_code ec;
+    pimpl->port.cancel(ec);
+    if(ec) setErrorStatus(true);
+    pimpl->port.close(ec);
+    if(ec) setErrorStatus(true);
+}
+
+void AsyncSerial::setErrorStatus(bool e)
+{
+    lock_guard<mutex> l(pimpl->errorMutex);
+    pimpl->error=e;
+}
+
+void AsyncSerial::setReadCallback(const boost::function<void (const char*, size_t)>& callback)
+{
+    pimpl->callback=callback;
+}
+
+void AsyncSerial::clearReadCallback()
+{
+    pimpl->callback.clear();
+}
+
+
+
+//
+//Class CallbackAsyncSerial
+//
+
+CallbackAsyncSerial::CallbackAsyncSerial(): AsyncSerial()
+{
+
+}
+
+CallbackAsyncSerial::CallbackAsyncSerial(const std::string& devname,
+        unsigned int baud_rate,
+        asio::serial_port_base::parity opt_parity,
+        asio::serial_port_base::character_size opt_csize,
+        asio::serial_port_base::flow_control opt_flow,
+        asio::serial_port_base::stop_bits opt_stop)
+        :AsyncSerial(devname,baud_rate,opt_parity,opt_csize,opt_flow,opt_stop)
+{
+
+}
+
+void CallbackAsyncSerial::setCallback(const boost::function<void (const char*, size_t)>& callback)
+{
+    setReadCallback(callback);
+}
+
+void CallbackAsyncSerial::clearCallback()
+{
+    clearReadCallback();
+}
+
+CallbackAsyncSerial::~CallbackAsyncSerial()
+{
+    clearReadCallback();
+}

3rd/boost_serial/AsyncSerial.h
@@ -1,0 +1,234 @@
+/*
+ * File:   AsyncSerial.h
+ * Author: Terraneo Federico
+ * Distributed under the Boost Software License, Version 1.0.
+ * Created on September 7, 2009, 10:46 AM
+ */
+
+#ifndef __ASYNCSERIAL_H__
+#define	__ASYNCSERIAL_H__
+
+#include <vector>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/thread.hpp>
+#include <boost/utility.hpp>
+#include <boost/function.hpp>
+#include <boost/shared_array.hpp>
+
+/**
+ * Used internally (pimpl)
+ */
+class AsyncSerialImpl;
+
+/**
+ * Asyncronous serial class.
+ * Intended to be a base class.
+ */
+class AsyncSerial: private boost::noncopyable
+{
+public:
+    AsyncSerial();
+
+    /**
+     * Constructor. Creates and opens a serial device.
+     * \param devname serial device name, example "/dev/ttyS0" or "COM1"
+     * \param baud_rate serial baud rate
+     * \param opt_parity serial parity, default none
+     * \param opt_csize serial character size, default 8bit
+     * \param opt_flow serial flow control, default none
+     * \param opt_stop serial stop bits, default 1
+     * \throws boost::system::system_error if cannot open the
+     * serial device
+     */
+    AsyncSerial(const std::string& devname, unsigned int baud_rate,
+        boost::asio::serial_port_base::parity opt_parity=
+            boost::asio::serial_port_base::parity(
+                boost::asio::serial_port_base::parity::none),
+        boost::asio::serial_port_base::character_size opt_csize=
+            boost::asio::serial_port_base::character_size(8),
+        boost::asio::serial_port_base::flow_control opt_flow=
+            boost::asio::serial_port_base::flow_control(
+                boost::asio::serial_port_base::flow_control::none),
+        boost::asio::serial_port_base::stop_bits opt_stop=
+            boost::asio::serial_port_base::stop_bits(
+                boost::asio::serial_port_base::stop_bits::one));
+
+    /**
+    * Opens a serial device.
+    * \param devname serial device name, example "/dev/ttyS0" or "COM1"
+    * \param baud_rate serial baud rate
+    * \param opt_parity serial parity, default none
+    * \param opt_csize serial character size, default 8bit
+    * \param opt_flow serial flow control, default none
+    * \param opt_stop serial stop bits, default 1
+    * \throws boost::system::system_error if cannot open the
+    * serial device
+    */
+    void open(const std::string& devname, unsigned int baud_rate,
+        boost::asio::serial_port_base::parity opt_parity=
+            boost::asio::serial_port_base::parity(
+                boost::asio::serial_port_base::parity::none),
+        boost::asio::serial_port_base::character_size opt_csize=
+            boost::asio::serial_port_base::character_size(8),
+        boost::asio::serial_port_base::flow_control opt_flow=
+            boost::asio::serial_port_base::flow_control(
+                boost::asio::serial_port_base::flow_control::none),
+        boost::asio::serial_port_base::stop_bits opt_stop=
+            boost::asio::serial_port_base::stop_bits(
+                boost::asio::serial_port_base::stop_bits::one));
+
+    /**
+     * \return true if serial device is open
+     */
+    bool isOpen() const;
+
+    /**
+     * \return true if error were found
+     */
+    bool errorStatus() const;
+
+    /**
+     * Close the serial device
+     * \throws boost::system::system_error if any error
+     */
+    void close();
+
+    /**
+     * Write data asynchronously. Returns immediately.
+     * \param data array of char to be sent through the serial device
+     * \param size array size
+     */
+    void write(const char *data, size_t size);
+
+     /**
+     * Write data asynchronously. Returns immediately.
+     * \param data to be sent through the serial device
+     */
+    void write(const std::vector<char>& data);
+
+    /**
+    * Write a string asynchronously. Returns immediately.
+    * Can be used to send ASCII data to the serial device.
+    * To send binary data, use write()
+    * \param s string to send
+    */
+    void writeString(const std::string& s);
+
+    virtual ~AsyncSerial()=0;
+
+    /**
+     * Read buffer maximum size
+     */
+    static const int readBufferSize=512;
+private:
+
+    /**
+     * Callback called to start an asynchronous read operation.
+     * This callback is called by the io_service in the spawned thread.
+     */
+    void doRead();
+
+    /**
+     * Callback called at the end of the asynchronous operation.
+     * This callback is called by the io_service in the spawned thread.
+     */
+    void readEnd(const boost::system::error_code& error,
+        size_t bytes_transferred);
+
+    /**
+     * Callback called to start an asynchronous write operation.
+     * If it is already in progress, does nothing.
+     * This callback is called by the io_service in the spawned thread.
+     */
+    void doWrite();
+
+    /**
+     * Callback called at the end of an asynchronuous write operation,
+     * if there is more data to write, restarts a new write operation.
+     * This callback is called by the io_service in the spawned thread.
+     */
+    void writeEnd(const boost::system::error_code& error);
+
+    /**
+     * Callback to close serial port
+     */
+    void doClose();
+
+    boost::shared_ptr<AsyncSerialImpl> pimpl;
+
+protected:
+
+    /**
+     * To allow derived classes to report errors
+     * \param e error status
+     */
+    void setErrorStatus(bool e);
+
+    /**
+     * To allow derived classes to set a read callback
+     */
+    void setReadCallback(const
+            boost::function<void (const char*, size_t)>& callback);
+
+    /**
+     * To unregister the read callback in the derived class destructor so it
+     * does not get called after the derived class destructor but before the
+     * base class destructor
+     */
+    void clearReadCallback();
+
+};
+
+/**
+ * Asynchronous serial class with read callback. User code can write data
+ * from one thread, and read data will be reported through a callback called
+ * from a separate thred.
+ */
+class CallbackAsyncSerial: public AsyncSerial
+{
+public:
+    CallbackAsyncSerial();
+
+    /**
+    * Opens a serial device.
+    * \param devname serial device name, example "/dev/ttyS0" or "COM1"
+    * \param baud_rate serial baud rate
+    * \param opt_parity serial parity, default none
+    * \param opt_csize serial character size, default 8bit
+    * \param opt_flow serial flow control, default none
+    * \param opt_stop serial stop bits, default 1
+    * \throws boost::system::system_error if cannot open the
+    * serial device
+    */
+    CallbackAsyncSerial(const std::string& devname, unsigned int baud_rate,
+        boost::asio::serial_port_base::parity opt_parity=
+            boost::asio::serial_port_base::parity(
+                boost::asio::serial_port_base::parity::none),
+        boost::asio::serial_port_base::character_size opt_csize=
+            boost::asio::serial_port_base::character_size(8),
+        boost::asio::serial_port_base::flow_control opt_flow=
+            boost::asio::serial_port_base::flow_control(
+                boost::asio::serial_port_base::flow_control::none),
+        boost::asio::serial_port_base::stop_bits opt_stop=
+            boost::asio::serial_port_base::stop_bits(
+                boost::asio::serial_port_base::stop_bits::one));
+
+    /**
+     * Set the read callback, the callback will be called from a thread
+     * owned by the CallbackAsyncSerial class when data arrives from the
+     * serial port.
+     * \param callback the receive callback
+     */
+    void setCallback(const boost::function<void (const char*, size_t)>& callback);
+
+    /**
+     * Removes the callback. Any data received after this function call will
+     * be lost.
+     */
+    void clearCallback();
+
+    virtual ~CallbackAsyncSerial();
+};
+
+#endif //ASYNCSERIAL_H

inc/core/popserial.hpp
@@ -1,15 +1,13 @@
 #ifndef __POP_SERIAL_HPP_
 #define __POP_SERIAL_HPP_
 
-
-#include <core/popsink.hpp>
-#include <core/popsource.hpp>
-
 #include <string>     // string function definitions
 #include <iostream>
-
-
 #include <boost/asio.hpp>
+
+#include "core/popsink.hpp"
+#include "core/popsource.hpp"
+#include "boost_serial/AsyncSerial.h"
 
 using namespace std;
 
@@ -18,164 +16,21 @@ using namespace std;
 namespace pop
 {
 
-
-// http://www.webalice.it/fede.tft/serial_port/serial_port.html
-// https://gitorious.org/serial-port/serial-port/source/03e161e0b788d593773b33006e01333946aa7e13:
-class SimpleSerial
-{
-
-
-public:
-    /**
-     * Constructor.
-     * \param port device name, example "/dev/tt yUSB0" or "COM4"
-     * \param baud_rate communication speed, example 9600 or 115200
-     * \throws boost::system::system_error if cannot open the
-     * serial device
-     */
-    SimpleSerial(std::string port, unsigned int baud_rate)
-    : io(), serial(io,port)
-    {
-        serial.set_option(boost::asio::serial_port_base::baud_rate(baud_rate));
-    }
-
-    /**
-     * Write a string to the serial device.
-     * \param s string to write
-     * \throws boost::system::system_error on failure
-     */
-    void writeString(std::string s)
-    {
-        boost::asio::write(serial,boost::asio::buffer(s.c_str(),s.size()));
-    }
-
-    void write(const char *data, size_t size)
-    {
-    	boost::asio::write(serial,boost::asio::buffer(data,size));
-    }
-
-    void close()
-    {
-    	boost::system::error_code ec;
-    	serial.close(ec);
-    }
-
-    /**
-     * Blocks until a line is received from the serial device.
-     * Eventual '\n' or '\r\n' characters at the end of the string are removed.
-     * \return a string containing the received line
-     * \throws boost::system::system_error on failure
-     */
-    std::string readLine()
-    {
-        //Reading data char by char, code is optimized for simplicity, not speed
-        using namespace boost;
-        char c;
-        std::string result;
-        for(;;)
-        {
-            asio::read(serial,asio::buffer(&c,1));
-            switch(c)
-            {
-                case '\r':
-                    break;
-                case '\n':
-                    return result;
-                default:
-                    result+=c;
-            }
-        }
-    }
-
-    unsigned char readChar()
-	{
-		//Reading data char by char, code is optimized for simplicity, not speed
-		using namespace boost;
-		unsigned c;
-
-		asio::read(serial,asio::buffer(&c,1));
-		return c;
-	}
-
-private:
-    boost::asio::io_service io;
-    boost::asio::serial_port serial;
-};
-
-
-
-
-
-
-
-
-
 class PopSerial : public PopSink<char>
 {
 public:
-	 PopSink<char> *tx;
-	 PopSource<char> rx; // serial receive generates characters
-	 string path;
-	 SimpleSerial handle;
+	PopSink<char> *tx;
+	PopSource<char> rx; // serial receive generates characters
+	string path;
 
+	// http://www.webalice.it/fede.tft/serial_port/serial_port.html
+	// https://gitorious.org/serial-port/serial-port/source/03e161e0b788d593773b33006e01333946aa7e13:
+	CallbackAsyncSerial handle;
 
-
-
-	PopSerial(std::string devicePath, unsigned baud = 115200) : PopSink<char>("PopSerialSink", 0), rx("PopSerialSource"), path(devicePath), handle(devicePath, baud)
-    {
-		tx = this;
-
-		// this ugly syntax is required if we want to use start_thread()
-		this->rx.set_loop_function(boost::bind(&PopSerial::run_loop,this));
-
-    }
-    void init()
-    {
-//    	 handle.writeString("Serial Boot\r\n");
-//    	while(1)
-//    	{
-//    		cout<<"Received : " << handle.readChar() << " : end" << endl;
-//    	}
-
-//		handle.close();
-    }
-
-    // this gets called over and over in a while(1), this function should not contain any sort of infinite loop itself
-    // return non-zero to exit loop
-    unsigned int run_loop()
-    {
-    	char* buf = rx.get_buffer(1);
-
-
-    	try {
-    		buf[0] = handle.readChar();
-    	}
-    	catch (const boost::system::system_error& e) {
-    		cout << "Exception in PopSerial: " << e.what() << endl;
-
-    		// return non-zero because something went wrong
-    		return 1;
-    	}
-
-
-//    	if( buf[0] == '\0')
-//    	{
-//    		cout << "NULL" << endl;
-//    	}
-//    	else
-//    	{
-//    		cout<<"Received : " << buf[0] << " (" << (int)buf[0] << ")" << endl;
-//    	}
-
-    	rx.process(1);
-
-    	return 0;
-    }
-
-    void process(const char* data, size_t size, const PopTimestamp* timestamp_data, size_t timestamp_size)
-    {
-    	handle.write(data, size);
-    }
+	PopSerial(std::string devicePath, unsigned baud = 115200);
+	void characters_received(const char*, size_t);
+	void init();
+	void process(const char* data, size_t size, const PopTimestamp* timestamp_data, size_t timestamp_size);
 };
 
 }

src/CMakeLists.txt
@@ -31,6 +31,7 @@ set(shared_SOURCES
 	../3rd/b64/b64.c
 	core/popjsonrpc.cpp
 	core/popchannelmap.cpp
+	../3rd/boost_serial/AsyncSerial.cpp
 )
 
 # S3P and Shannon sources
@@ -61,6 +62,7 @@ set(gravatino_SOURCES
 	core/popartemisrpc.cpp
 	core/pops3prpc.cpp
 	core/poppackethandler.cpp
+	core/popserial.cpp
 	dsp/prota/popsparsecorrelate.c
 )
 

src/app/gravitino.cpp
@@ -92,7 +92,6 @@ int main(int argc, char *argv[])
 		PopSerial uart0("/dev/ttyUSB0", 115200);
 		rpc.tx.connect(uart0);
 		uart0.rx.connect(rpc);
-		uart0.rx.start_thread();
 		rpc.send_reset();
 		boost::posix_time::milliseconds one_second(1000);
 		for(j = 0; j < 4; j++)
@@ -120,7 +119,7 @@ int main(int argc, char *argv[])
 
 	uart0.rx.connect(rpc);
 	rpc.tx.connect(uart0);
-	uart0.rx.start_thread();
+
 
 	// Send a "set_role_base_station" RPC to the Artemis board to force it into
 	// base station mode.
@@ -135,7 +134,6 @@ int main(int argc, char *argv[])
 	PopParseGPS gps(1);
 //	PopSerial uart4("/dev/tty4", 4800);
 //	uart4.rx.connect(gps);
-//	uart4.rx.start_thread();
 
 	PopNetwork<char> json(0, Config::get<std::string>("basestation_s3p_ip"), Config::get<int>("basestation_s3p_port"), 0);
 

src/core/popserial.cpp
@@ -1,0 +1,33 @@
+#include "core/popserial.hpp"
+
+using namespace std;
+
+namespace pop
+{
+
+
+PopSerial::PopSerial(std::string devicePath, unsigned baud) : PopSink<char>("PopSerialSink", 0), rx("PopSerialSource"), path(devicePath), handle(devicePath, baud)
+{
+	// note the weird _1 and _2. see http://stackoverflow.com/a/2304211/836450
+	handle.setCallback(boost::bind(&PopSerial::characters_received,this,_1,_2));
+
+	tx = this;
+}
+
+void PopSerial::init()
+{
+}
+
+
+void PopSerial::characters_received(const char *data, size_t size)
+{
+	rx.process(data, size);
+}
+
+
+void PopSerial::process(const char* data, size_t size, const PopTimestamp* timestamp_data, size_t timestamp_size)
+{
+	handle.write(data, size);
+}
+
+}